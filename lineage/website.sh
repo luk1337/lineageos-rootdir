5aa58d3af5e05bb1d79b5000292a8f61bfd75a4e
LuK1337 <priv.luk@gmail.com>
2023-11-02 14:26:34 +0100

    Add wiki/www scripts

---
+++ lineage/website.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if ! docker image exists lineageos/www; then
+    echo "
+    FROM docker.io/ruby:2.6
+
+    WORKDIR /pwd
+    RUN mkdir -p /.repo/projects/lineage/website.git
+    RUN gem install bundler -v 2.3.26
+    RUN bundle install
+    " | docker build -t lineageos/www -v $PWD:/pwd:Z -
+fi
+
+docker run -v $PWD:/pwd:Z -p 4000:4000 -w /pwd -ti lineageos/www bundle exec jekyll serve --host 0.0.0.0 --future $@
