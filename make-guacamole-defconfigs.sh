ee9cc82503a146906548d5e93785e8fda87b56ea
LuK1337 <priv.luk@gmail.com>
2025-06-08 09:31:08 +0200

    Add defconfig build script for guacamole

---
+++ make-guacamole-defconfigs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function make-kernel-clang() {
+    ARCH=arm64 CC=clang LD=ld.lld LLVM=1 make O=../out $@
+}
+
+make-kernel-clang vendor/sm8150-perf_defconfig
+mv ../out/.config{,_a}
+
+make-kernel-clang vendor/sm8150-perf_defconfig vendor/oplus.config
+mv ../out/.config{,_b}
+scripts/diffconfig -m ../out/.config_{a,b} > arch/arm64/configs/vendor/oplus.config
